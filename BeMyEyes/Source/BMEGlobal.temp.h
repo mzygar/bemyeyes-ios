5afc4298715dee1ad8ac41800038d712fa4af34c
Simon Støvring <simonstoevring@gmail.com>
2014-03-23 22:01:17 +0100

    Preparations

---
+++ BeMyEyes/Source/BMEGlobal.temp.h
@@ -0,0 +1,37 @@
+//
+//  BMEGlobal.h
+//  BeMyEyes
+//
+//  Created by Simon Støvring on 22/02/14.
+//  Copyright (c) 2014 Be My Eyes. All rights reserved.
+//
+
+#ifndef BMEGlobal_h
+#define BMEGlobal_h
+
+#define BMEAPIBaseUrl @"http://devapi.bemyeyes.org/"
+#define BMEAPIUsername @""
+#define BMEAPIPassword @""
+
+#define BMEOpenTokAPIKey @""
+
+#define BMESecuritySalt @""
+
+#define BMEFacebookAppId @""
+
+#define BMEErrorDomain @"org.bemyeyes.BeMyEyes"
+
+#define BMEMainControllerIdentifier @"Main"
+#define BMEMainBlindControllerIdentifier @"MainBlind"
+#define BMEMainHelperControllerIdentifier @"MainHelper"
+#define BMEMenuControllerIdentifier @"Menu"
+#define BMECallControllerIdentifier @"Call"
+
+#define BMEDidLogOutNotification @"BMEDidLogOutNotification"
+
+typedef NS_ENUM(NSInteger, BMERole) {
+    BMERoleBlind = 0,
+    BMERoleHelper,
+};
+
+#endif
