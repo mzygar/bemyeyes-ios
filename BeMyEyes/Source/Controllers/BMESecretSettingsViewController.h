44d53f25347447fcfa8664029d792967ca3140cb
Simon Støvring <simonstoevring@gmail.com>
2014-04-18 15:45:15 +0200

    Prepares for secret settings

---
+++ BeMyEyes/Source/Controllers/BMESecretSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BMESecretSettingsViewController.h
+//  BeMyEyes
+//
+//  Created by Simon Støvring on 18/04/14.
+//  Copyright (c) 2014 Be My Eyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMESecretSettingsViewController : UIViewController
+
+@end
---
+++ BeMyEyes/Source/Controllers/BMESecretSettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  BMESecretSettingsViewController.m
+//  BeMyEyes
+//
+//  Created by Simon Støvring on 18/04/14.
+//  Copyright (c) 2014 Be My Eyes. All rights reserved.
+//
+
+#import "BMESecretSettingsViewController.h"
+
+@implementation BMESecretSettingsViewController
+
+@end
