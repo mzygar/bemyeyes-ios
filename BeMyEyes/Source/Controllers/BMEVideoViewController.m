f226dc94488c320064e9d9705b70e6b0f2fe5571
Simon Støvring <simonstoevring@gmail.com>
2014-08-24 12:17:23 +0200

    Mute switch is ignored while video is playing

--- BeMyEyes/Source/Controllers/BMEVideoViewController.m
+++ BeMyEyes/Source/Controllers/BMEVideoViewController.m
@@ -8,10 +8,12 @@
 
 #import "BMEVideoViewController.h"
 #import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface BMEVideoViewController ()
 @property (strong, nonatomic) MPMoviePlayerController *moviePlayerController;
 @property (assign, nonatomic) BOOL statusBarVisible;
+@property (strong, nonatomic) NSString *defaultAudioCategory;
 @end
 
 @implementation BMEVideoViewController
@@ -23,6 +25,7 @@
     if (self = [super init]) {
         if (contentURL) {
             _moviePlayerController = [[MPMoviePlayerController alloc] initWithContentURL:contentURL];
+            [self ignoreMuteSwitch];
         }
     }
     
@@ -70,6 +73,8 @@
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     
+    [self resetAudioCategory];
+    
     if (self.statusBarVisible) {
         [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
     }
@@ -86,11 +91,35 @@
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     
+    [self resetAudioCategory];
+    
     _moviePlayerController = nil;
+    _defaultAudioCategory = nil;
 }
 
 #pragma mark -
 #pragma mark Private Methods
+
+- (void)ignoreMuteSwitch {
+    self.defaultAudioCategory = [[AVAudioSession sharedInstance] category];
+    [self useAudioCategory:AVAudioSessionCategoryPlayback];
+}
+
+- (void)resetAudioCategory {
+    if (self.defaultAudioCategory) {
+        [self useAudioCategory:self.defaultAudioCategory];
+        self.defaultAudioCategory = nil;
+    }
+}
+
+- (void)useAudioCategory:(NSString *)audioCategory {
+    NSError *error = nil;
+    [[AVAudioSession sharedInstance] category];
+    BOOL success = [[AVAudioSession sharedInstance] setCategory:audioCategory error:&error];
+    if (!success) {
+        NSLog(@"Could not set audio category to '%@': %@", audioCategory, error);
+    }
+}
 
 - (void)playVideo {
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playbackFinished:) name:MPMoviePlayerPlaybackDidFinishNotification object:self.moviePlayerController];
