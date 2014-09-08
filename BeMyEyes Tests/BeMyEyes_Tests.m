42a11c9bcdacd71bcb11f43aa454bdcbc31fa8ae
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-09-08 13:01:27 +0200

    added test cases, they dont work yet since they are async

--- BeMyEyes Tests/BeMyEyes_Tests.m
+++ BeMyEyes Tests/BeMyEyes_Tests.m
@@ -28,7 +28,7 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testRegisterDevice
 {
    NSString *normalizedDeviceToken = @"1234";
     // Register new device token
@@ -39,5 +39,45 @@
         }
     }];
 }
+- (void)testCreateUserWithEmail
+{
+    NSString *email = @"someone@example.com";
+    NSString *password = @"Password1";
+    NSString *firstName = @"FirstName";
+    NSString *lastName = @"LastName";
+    [[BMEClient sharedClient] createUserWithEmail:email password:password firstName:firstName lastName:lastName role:BMERoleHelper completion:^(BOOL success, NSError *error) {
+        XCTAssertTrue(success && !error);
+    }];
+}
 
+- (void)testCreateUserAndLogin
+{
+    NSString *normalizedDeviceToken = @"1234";
+    NSString *email = @"someone@example.com";
+    NSString *password = @"Password1";
+    NSString *firstName = @"FirstName";
+    NSString *lastName = @"LastName";
+    // Register new device token
+    [[BMEClient sharedClient] registerDeviceWithAbsoluteDeviceToken:normalizedDeviceToken active:YES production:BMEIsProductionOrAdHoc completion:^(BOOL success, NSError *error) {
+        
+        if (error) {
+            XCTFail (@"Failed registering device: %@", error);
+            NSLog(@"Failed registering device: %@", error);
+        }
+    }];
+    
+    [[BMEClient sharedClient] createUserWithEmail:email password:password firstName:firstName lastName:lastName role:BMERoleHelper completion:^(BOOL success, NSError *error) {
+        XCTAssertTrue(success && !error);
+    }];
+    
+    [[BMEClient sharedClient] loginWithEmail:email password:password deviceToken:normalizedDeviceToken success:^(BMEToken *token) {
+        //XCTSuccess(@"");
+    } failure:^(NSError *error) {
+        XCTFail("Unable to login");
+        NSLog(@"Unable to login");
+        }
+     ];
+}
+
+     
 @end
